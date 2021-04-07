e5b9b8fd7ce17b8f39d8e5cd66b63a0b78a85241
LordOFGamemode <62309374+LordOFGamemode@users.noreply.github.com>
2021-04-07 22:47:22 +0200

    Projektdateien hinzufügen.

---
+++ Server/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            while (true)
+            {
+                string inp = Console.ReadLine();
+                if (inp == "exit")
+                {
+                    break;
+                }
+            }
+        }
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AccaptCallback), null);
+
+        }
+
+        private static void AccaptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client Connected.");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AccaptCallback), null);
+        }
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int receved = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[receved];
+            Array.Copy(_buffer, dataBuf, receved);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("[Client => Server] " + text);
+
+            if (text.ToLower() == "time")
+            {
+                byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+                
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                _serverSocket.BeginAccept(new AsyncCallback(AccaptCallback), null);
+            }else
+            {
+                
+            }
+        }
+        
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+}
